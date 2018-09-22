73592f0d3af7f4bd8b20d906f19e7597d5c68fa4
ReynelG <43390647+ReynelG@users.noreply.github.com>
2018-09-22 12:15:35 -0600

    Update PWM.h

--- PWM.h
+++ PWM.h
@@ -1,10 +1,49 @@
-
 class PWM {
-	const char *Export = "/sys/class/pwm/pwmchip0/export";
-	const char *Period = "/sys/class/pwm/pwmchip0/pwm-0:0/period";
-	const char *Enable = "/sys/class/pwm/pwmchip0/pwm-0:0/enable";
-	const char *Duty_cycle = "/sys/class/pwm/pwmchip0/pwm-0:0/duty_cycle";public:
-	PWM(){
-		
+	const char *Export_dir = "/sys/class/pwm/pwmchip0/export";
+	const char *Period_dir = "/sys/class/pwm/pwmchip0/pwm-0:0/period";
+	const char *Enable_dir = "/sys/class/pwm/pwmchip0/pwm-0:0/enable";
+	const char *Duty_cycle_dir = "/sys/class/pwm/pwmchip0/pwm-0:0/duty_cycle";
+	char Period[10];
+	char Duty_Cycle[10]
+	public:
+	PWM(char P[10], char DC[10])
+	{
+		FILE PWM = NULL;
+		if((PWM = fopen(Export_dir, "r+")) != NULL)
+		{
+			fwrite("0", sizeof(char), 1, PWM);
+			fclose(PWM);
+		}
+		if((PWM = fopen(Enable_dir, "r+")) != NULL)
+		{
+			fwrite("1", sizeof(char), 1, PWM);
+			fclose(PWM);
+		}
+		Period = P;
+		Duty_Cycle = DC;
 	};
+	void Change()
+	{
+		FILE PWM = NULL;
+		if((PWM = fopen(Period_dir, "r+")) != NULL)
+		{
+			fwrite(Period, sizeof(char), 7, PWM);
+			fclose(PWM);
+		}
+		if((PWM = fopen(Duty_cycle_dir, "r+")) != NULL)
+		{
+			fwrite(Duty_Cycle, sizeof(char), 7, PWM);
+			fclose(PWM);
+		}
+	};
+};
+class Backlight{
+	char Duty[7];
+	public: 
+	Backlight(char Percent[3]){
+		Duty = Percent||"0000";
+	}
+	PWM Brightness;
+	Brightness.PWM("1000000",Percent);
+	
 };
