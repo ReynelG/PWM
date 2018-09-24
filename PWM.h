ec5929747a2a73c20210beb40c7ca8d2908b4d0f
ReynelG <43390647+ReynelG@users.noreply.github.com>
2018-09-24 11:56:02 -0600

    Update PWM.h

--- PWM.h
+++ PWM.h
@@ -1,50 +1,21 @@
+#ifndef _PWM_H_
+#define _PWM_H_
+
+#define Export_dir "/sys/class/pwm/pwmchip0/export"
+#define Period_dir "/sys/class/pwm/pwmchip0/pwm-0:0/period"
+#define En_dir "/sys/class/pwm/pwmchip0/pwm-0:0/enable"
+#define DC_dir "/sys/class/pwm/pwmchip0/pwm-0:0/duty_cycle"
+
 class PWM {
-	const char *Export_dir = "/sys/class/pwm/pwmchip0/export";
-	const char *Period_dir = "/sys/class/pwm/pwmchip0/pwm-0:0/period";
-	const char *Enable_dir = "/sys/class/pwm/pwmchip0/pwm-0:0/enable";
-	const char *Duty_cycle_dir = "/sys/class/pwm/pwmchip0/pwm-0:0/duty_cycle";
-	char Period[10];
-	char Duty_Cycle[10]
-	public:
-	PWM(char P[10], char DC[10])
-	{
-		FILE PWM = NULL;
-		if((PWM = fopen(Export_dir, "r+")) != NULL)
-		{
-			fwrite("0", sizeof(char), 1, PWM);
-			fclose(PWM);
-		}
-		if((PWM = fopen(Enable_dir, "r+")) != NULL)
-		{
-			fwrite("1", sizeof(char), 1, PWM);
-			fclose(PWM);
-		}
-		Period = P;
-		Duty_Cycle = DC;
-	};
-	void Change()
-	{
-		FILE PWM = NULL;
-		if((PWM = fopen(Period_dir, "r+")) != NULL)
-		{
-			fwrite(Period, sizeof(char), 7, PWM);
-			fclose(PWM);
-		}
-		if((PWM = fopen(Duty_cycle_dir, "r+")) != NULL)
-		{
-			fwrite(Duty_Cycle, sizeof(char), 7, PWM);
-			fclose(PWM);
-		}
-	};
-};
-class Backlight{
-	char Duty[7];
-	public: 
-	Backlight(char Percent[3]){
-  		strcpy (Duty,Percent);
-  		strcat (Duty,"0000");
-	}
-	PWM Brightness;
-	Brightness.PWM("1000000",Duty);
+	std::string Period, Duty_Cycle;
 	
+	public:
+	PWM(std::string p, std::string dc): Period(p),Duty_Cycle(dc){};
+	};
+class Backlight{
+	std::string Percent;
+	public: 
+	Backlight(std::string p): Percent(p){};
+	PWM Brightness;	
 };
+#endif
