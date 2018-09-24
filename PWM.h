24efffa0e8aab902dff70173a2b1c826c3cbf6f0
ReynelG <43390647+ReynelG@users.noreply.github.com>
2018-09-24 15:16:14 -0600

    Update PWM.h

--- PWM.h
+++ PWM.h
@@ -1,21 +1,64 @@
 #ifndef _PWM_H_
 #define _PWM_H_
 
+#include <stdio.h>
+#include <unistd.h>
+
 #define Export_dir "/sys/class/pwm/pwmchip0/export"
 #define Period_dir "/sys/class/pwm/pwmchip0/pwm-0:0/period"
 #define En_dir "/sys/class/pwm/pwmchip0/pwm-0:0/enable"
 #define DC_dir "/sys/class/pwm/pwmchip0/pwm-0:0/duty_cycle"
 
+
+
 class PWM {
 	std::string Period, Duty_Cycle;
 	
 	public:
-	PWM(std::string p, std::string dc): Period(p),Duty_Cycle(dc){};
+	// Default values, 50% duty cycle
+	PWM():Period("1000000"),Duty_Cycle("500000"){
+		FILE * PWM_ = NULL;
+		if((PWM_ = fopen(Export_dir, "w")) != NULL)
+		{
+		fwrite("0", sizeof(char), 1, PWM);
+		fclose(PWM_);
+		}
+		while((PWM_ = fopen(Period, "w")) == NULL)
+		{
+			usleep(1);
+		}
+		
+	}; // Default value
+	PWM(std::string p, std::string dc): Period(p),Duty_Cycle(dc){
+		FILE * PWM_ = NULL;
+		if((PWM_ = fopen(Export_dir, "w")) != NULL)
+		{
+		fwrite("0", sizeof(char), 1, PWM);
+		fclose(PWM_);
+		}
+		while((PWM_ = fopen(Period_dir, "r+")) == NULL)
+		{
+			usleep(1);
+		}
+		
+	}; // Change value, nanoseconds
+	void run(){
+		FILE * PWM_ = NULL;
+		if((PWM_ = fopen(Period_dir, "w")) != NULL)
+		{
+			fwrite(Period, sizeof(std::string), 1, PWM_);
+			fclose(PWM_);
+		}
+		if((PWM_ = fopen(En_dir, "w")) != NULL)
+		{
+			fwrite("1", sizeof(char), 1, PWM_);
+			fclose(PWM_);
+		}
+		if((PWM_ = fopen(DC_dir, "w")) != NULL)
+		{
+			fwrite(Duty_Cycle, sizeof(std::string), 1, PWM_);
+			fclose(PWM_);
+		}
+		
 	};
-class Backlight{
-	std::string Percent;
-	public: 
-	Backlight(std::string p): Percent(p){};
-	PWM Brightness;	
-};
 #endif
