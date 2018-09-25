bc93191b98fc4315dc444ff8bfeada670c5dc2b3
ReynelG <43390647+ReynelG@users.noreply.github.com>
2018-09-25 16:16:59 -0600

    Update PWM.h

--- PWM.h
+++ PWM.h
@@ -7,21 +7,19 @@
 #include <cstring>
 
 // PWM driver directories
-#define Export_dir "/sys/class/pwm/pwmchip0/export"
-#define Period_dir "/sys/class/pwm/pwmchip0/pwm-0:0/period"
-#define En_dir "/sys/class/pwm/pwmchip0/pwm-0:0/enable"
-#define DC_dir "/sys/class/pwm/pwmchip0/pwm-0:0/duty_cycle"
+
 
 using namespace std;
 
 
 class PWM {
 	private:
-		std::string Period;		//PWM period, nanoseconds
-		std::string Duty_Cycle;		//PWM Duty cycle, nanoseconds
-	
+		std::string Period;						//PWM period, nanoseconds
+		std::string Duty_Cycle;						//PWM Duty cycle, nanoseconds
+		std::string Port;						//PWM Port (3 available)
+		std::string Channel;						//PWM Channel (2 for every port)
 	public:
-		PWM(std::string, std::string);	//Constructor, creates the directories.
-		void run();			//Starts
+		PWM(std::string, std::string, std::string, std::string);	//Constructor, creates the directories.
+		void run();							//Starts
 	};
 #endif
