884bd1579fea2c7082c84f7a7d41cc9fae6f05bf
ReynelG <43390647+ReynelG@users.noreply.github.com>
2018-09-25 11:54:21 -0600

    Update PWM.cpp

--- PWM.cpp
+++ PWM.cpp
@@ -4,55 +4,41 @@
 
  
 
-	PWM::PWM(std::string p, std::string dc):Period(p),Duty_Cycle(dc){
-		FILE * PWM_ = NULL;
-		if((PWM_ = fopen(Export_dir, "w")) != NULL)
-		{
+PWM::PWM(std::string p, std::string dc):Period(p),Duty_Cycle(dc){
+	FILE * PWM_ = NULL;
+	if((PWM_ = fopen(Export_dir, "w")) != NULL)
+	{
 		fwrite("0", sizeof(char), 1, PWM_);
 		fclose(PWM_);
-		}
-		while((PWM_ = fopen(Period_dir, "r+")) == NULL)
-		{
-			usleep(1);
-		}
 	}
-	void PWM::run(){
-		char * P = new char [Period.length()+1];
-		std::strcpy (P, Period.c_str());
-		char * DC = new char [Duty_Cycle.length()+1];
-		std::strcpy (DC, Duty_Cycle.c_str());
-		
-		FILE * PWM_ = NULL;
-		
-		if((PWM_ = fopen(Period_dir, "w")) != NULL)
-		{
-			fwrite(P, sizeof(char), Period.length(), PWM_);
-			fclose(PWM_);
-		}
-		if((PWM_ = fopen(En_dir, "w")) != NULL)
-		{
-			fwrite("1", sizeof(char), 1, PWM_);
-			fclose(PWM_);
-		}
-		if((PWM_ = fopen(DC_dir, "w")) != NULL)
-		{
-			fwrite(DC, sizeof(char), 7, PWM_);
-			fclose(PWM_);
-		}
-		delete[] P;
-		delete[] DC;
+	while((PWM_ = fopen(Period_dir, "r+")) == NULL)
+	{
+		usleep(1);
 	}
-int main(){
-	// Default Value
-	PWM init("1000000","500000");
-	init.run();
+}
+void PWM::run(){
+	char * P = new char [Period.length()+1];
+	std::strcpy (P, Period.c_str());
+	char * DC = new char [Duty_Cycle.length()+1];
+	std::strcpy (DC, Duty_Cycle.c_str());
 	
-	// Program for brightness
-	std::string Percent;
-	cin>>Percent;
-	Percent = Percent + "0000";
-	cout<<Percent;
-	PWM Brightness("1000000",Percent);
-	Brightness.run();
-	return(0);
+	FILE * PWM_ = NULL;
+		
+	if((PWM_ = fopen(Period_dir, "w")) != NULL)
+	{
+		fwrite(P, sizeof(char), Period.length(), PWM_);
+		fclose(PWM_);
+	}
+	if((PWM_ = fopen(En_dir, "w")) != NULL)
+	{
+		fwrite("1", sizeof(char), 1, PWM_);
+		fclose(PWM_);
+	}
+	if((PWM_ = fopen(DC_dir, "w")) != NULL)
+	{
+		fwrite(DC, sizeof(char), 7, PWM_);
+		fclose(PWM_);
+	}
+	delete[] P;
+	delete[] DC;
 }
