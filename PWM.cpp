dc2603e3c208fa6dc711ea80f49b85fd86e53271
ReynelG <43390647+ReynelG@users.noreply.github.com>
2018-09-26 16:16:59 -0600

    Update PWM.cpp

--- PWM.cpp
+++ PWM.cpp
@@ -1,15 +1,17 @@
 #include <string.h>
+#include <fcntl.h>
 #include "PWM.h"
 
 // Defines constructor, Sets directories.
 // Ports available: 0,2,4. Channels available: 0,1. (6 total).
 PWM::PWM(std::string p, std::string dc, std::string port, std::string ch):Period(p),Duty_Cycle(dc),Port(port),Channel(ch){
 	
-	FILE * PWM_ = NULL;
+	// FILE * PWM_ = NULL;
 	std::string dir = "/sys/class/pwm/pwmchip"+Port+"/pwm-"+Port+":"+Channel+"/period";
 	char * Period_dir = new char [dir.length()+1];
 	std::strcpy (Period_dir, dir.c_str());
-	if(access(Period_dir,F_OK) != 0)
+	int PWM_ = open(Period_dir, O_RDONLY);
+	if(PWM_ == -1)
 	{
 		// Export creates directories for chosen port and channel
 		std::string Export = "/sys/class/pwm/pwmchip"+Port+"/export"; 
@@ -17,35 +19,36 @@ PWM::PWM(std::string p, std::string dc, std::string port, std::string ch):Period
 		std::strcpy (Export_dir, Export.c_str());
 		char * CH = new char[Channel.length()+1];
 		std::strcpy (CH, Channel.c_str());
-		if((PWM_ = fopen(Export_dir, "w")) != NULL)
+		
+		int PWM_ = open(Export_dir,O_WRONLY);
+		if((PWM_ != -1)
 		{
-			fwrite(CH, sizeof(char), 1, PWM_);
-			fclose(PWM_);
+			int n =write(PWM_, CH, sizeof(char));
+			close(PWM_);
 		}
 		delete[] Export_dir;
 		delete[] CH;
+	}
+	else
+	{
+		close(PWM_);
 	}
 	delete[] Period_dir;
 }
 void PWM::run(){
 	
-	FILE * PWM_ = NULL;
+	// FILE * PWM_ = NULL;
 	
 	std::string dir = "/sys/class/pwm/pwmchip"+Port+"/pwm-"+Port+":"+Channel+"/period";
 	char * Period_dir = new char [dir.length()+1];
 	std::strcpy (Period_dir, dir.c_str());		
-	while(access(Period_dir,W_OK) != 0)
-	{
-		usleep(1);
-	}
+	int PWM_ = open(Period_dir,O_WRONLY);
 	char * P = new char [Period.length()+1];
 	std::strcpy (P, Period.c_str());
-	if((PWM_ = fopen(Period_dir, "w")) != NULL)
+	if(PWM_ != -1)
 	{
-		while((fwrite(P, sizeof(char), Period.length(), PWM_)) != Period.length())
-		{
-		};
-		fclose(PWM_);
+		write(PWM_, P, Period.length(),);
+		close(PWM_);
 	}
 	delete[] Period_dir;	
 	delete[] P;
@@ -53,10 +56,11 @@ void PWM::run(){
 	dir = "/sys/class/pwm/pwmchip"+Port+"/pwm-"+Port+":"+Channel+"/enable";
 	char * En_dir = new char [dir.length()+1];
 	std::strcpy (En_dir, dir.c_str());
-	if((PWM_ = fopen(En_dir, "w")) != NULL)
+	int PWM_ = open(En_dir,O_WRONLY);
+	if((PWM_ != -1)
 	{
-		fwrite("1", sizeof(char), 1, PWM_);
-		fclose(PWM_);
+		write(PWM_,"1", sizeof(char));
+		close(PWM_);
 	}
 	delete[] En_dir;
 	
@@ -66,10 +70,11 @@ void PWM::run(){
 	std::strcpy (DC_dir, dir.c_str());
 	char * DC = new char [Duty_Cycle.length()+1];
 	std::strcpy (DC, Duty_Cycle.c_str());
-	if((PWM_ = fopen(DC_dir, "w")) != NULL)
+	int PWM_ = open(DC_dir,O_WRONLY);
+	if((PWM_ != -1)
 	{
-		fwrite(DC, sizeof(char), Duty_Cycle.length(), PWM_);
-		fclose(PWM_);
+		write(PWM_, DC, Duty_Cycle.length());
+		close(PWM_);
 	}
 	delete[] DC_dir;
 	delete[] DC;
