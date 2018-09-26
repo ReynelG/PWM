8c217c55e418b67445086c8481307183e9c89ff4
ReynelG <43390647+ReynelG@users.noreply.github.com>
2018-09-26 13:39:05 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,9 +5,8 @@ int main(){
 	// Program for brightness
 	std::string Percent;
 	cin>>Percent;
-	Percent = Percent + "0000";
-	cout<<Percent;
-	PWM Brightness("1000000",Percent,"0","0");
+	Percent = Percent + "0000"; // PWM entries are in nanoseconds, multiplier.
+	PWM Brightness("1000000",Percent,"0","0"); //LCD connected to channel 0 on port 0.
 	Brightness.run();
 	return(0);
 }
