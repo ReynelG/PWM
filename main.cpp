c70c3d45bec4624c7697cc93f620ab654cb5deb6
ReynelG <43390647+ReynelG@users.noreply.github.com>
2018-09-26 09:54:38 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(){
 	cin>>Percent;
 	Percent = Percent + "0000";
 	cout<<Percent;
-	PWM Brightness("1000000",Percent);
+	PWM Brightness("1000000",Percent,"0","0");
 	Brightness.run();
 	return(0);
 }
