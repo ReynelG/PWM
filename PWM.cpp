b62fc63042794220e2de60d9edd70e0bdd9a4014
ReynelG <43390647+ReynelG@users.noreply.github.com>
2018-09-26 13:00:38 -0600

    Update PWM.cpp

--- PWM.cpp
+++ PWM.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <string.h>
 #include "PWM.h"
 
@@ -6,31 +7,27 @@
 PWM::PWM(std::string p, std::string dc, std::string port, std::string ch):Period(p),Duty_Cycle(dc),Port(port),Channel(ch){
 	
 	FILE * PWM_ = NULL;
-	
-	// Export creates directories for chosen port and channel
-	std::string Export = "/sys/class/pwm/pwmchip"+Port+"/export"; 
-	char * Export_dir = new char [Export.length()+1];
-	std::strcpy (Export_dir, Export.c_str());
-	char * CH = new char[Channel.length()+1];
-	std::strcpy (CH, Channel.c_str());
-	if((PWM_ = fopen(Export_dir, "w")) != NULL)
-	{
-		fwrite(CH, sizeof(char), 1, PWM_);
-		fclose(PWM_);
-	}
-	delete[] Export_dir;
-	delete[] CH;
-	
-	//Checks if new directory exist.
 	std::string dir = "/sys/class/pwm/pwmchip"+Port+"/pwm-"+Port+":"+Channel+"/period";
 	char * Period_dir = new char [dir.length()+1];
-	std::strcpy (Period_dir, dir.c_str());	
-	while(PWM_ == NULL || PWM_ == 0)
+	std::strcpy (Period_dir, dir.c_str());
+	while((PWM_ = fopen(Period_dir, "w")) == NULL)
 	{
-		PWM_ = fopen(Period_dir, "r+");
-		usleep(1);
+		cout<<(PWM_ = fopen(Period_dir, "w"));
+		fclose(PWM_);
+		// Export creates directories for chosen port and channel
+		std::string Export = "/sys/class/pwm/pwmchip"+Port+"/export"; 
+		char * Export_dir = new char [Export.length()+1];
+		std::strcpy (Export_dir, Export.c_str());
+		char * CH = new char[Channel.length()+1];
+		std::strcpy (CH, Channel.c_str());
+		if((PWM_ = fopen(Export_dir, "w")) != NULL)
+		{
+			fwrite(CH, sizeof(char), 1, PWM_);
+			fclose(PWM_);
+		}
+		delete[] Export_dir;
+		delete[] CH;
 	}
-	fclose(PWM_);
 	delete[] Period_dir;
 }
 void PWM::run(){
