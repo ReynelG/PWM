5ddbca0d2e9e25ecbfa6b31b35690e3a0a7c9d45
ReynelG <43390647+ReynelG@users.noreply.github.com>
2018-09-26 16:56:56 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(){
 	std::string Percent;
 	cin>>Percent;
 	Percent = Percent + "0000"; // PWM entries are in nanoseconds, multiplier.
-	PWM Brightness("1000000",Percent,"0","0"); //LCD connected to channel 0 on port 0.
-	Brightness.run();
+	PWM Brightness("0","0"); //LCD connected to channel 0 on port 0.
+	Brightness.run("1000000",Percent); // 1kHz signal, Percent duty cycle.
 	return(0);
 }
